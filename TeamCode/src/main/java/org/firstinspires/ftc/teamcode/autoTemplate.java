2359bcbc2d404d7a4da24bd84b7ba1946a2a121c
afriedman26 <arifriedman26@gmail.com>
2024-12-30 19:22:42 -0500

    auto is like halfway done itsg if this breaks again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTemplate.java
@@ -2,9 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import  com.qualcomm.robotcore.hardware.Servo;
 @Autonomous(name = "autoTemplate")
 public class autoTemplate extends LinearOpMode {
@@ -81,27 +79,23 @@ public class autoTemplate extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            afterTransfer1();
-            drive(300, 300, 0.6);
-            strafe(1000, 0.6);
-            raiseSlides();
-            sleep(600);
+            setUpAuto();
+            drive(-300, -300, 0.6);
+            strafe(-1100, 0.6);
             turnRight(500, 0.6);
-            drive(-270, -270, .6);
+            drive(300,300,0.6);
+            raiseSlides();
             transfer2();
-            sleep(500);
-            drive(300, 300, .6);
-            afterTransfer1();
             lowerSlides();
-            turnRight(550, 0.6);
-            strafe(750, 0.6);
-            drive(900, 800, 0.6);
+            setUpAuto();
+            turnRight(500,0.6);
+            strafe(-750, 0.6);
+            drive(-900, -900, 0.6);
             strafe(1390, 0.6);
             turnRight(2130, 0.6);
             drive(-500, -500, 0.6);
             raiseSlides();
             sleep(1700);
-
 
             telemetry.addData("armServoR Position: ", armServoR.getPosition());
             telemetry.addData("armServoL Position: ", armServoR.getPosition());
@@ -111,6 +105,7 @@ public class autoTemplate extends LinearOpMode {
             telemetry.addData("Wrist Position", wristServo.getPosition());
             telemetry.addData("top claw", topClaw.getPosition());
             telemetry.update();
+            break;
 
         }
 
@@ -234,28 +229,16 @@ public class autoTemplate extends LinearOpMode {
     public void raiseSlides() {
         slideMotorL.setPower(.4);
         slideMotorR.setPower(-.4);
-        if (gamepad1.a) {
-            slideMotorL.setPower(0);
-            slideMotorR.setPower(0);
-        }
-        sleep(3700);
-        slideMotorL.setPower(0);
-        slideMotorR.setPower(0);
-
+        sleep(3600);
+        slideMotorL.setPower(.09);
+        slideMotorR.setPower(-0.09);
     }
 
-    public void lowerSlides() {
-        slideMotorL.setPower(-.4);
-        slideMotorR.setPower(.4);
-        if (gamepad1.b) {
-            slideMotorL.setPower(0);
-            slideMotorR.setPower(0);
+        public void lowerSlides() {
+            slideMotorL.setPower(-.4);
+            slideMotorR.setPower(.4);
+            sleep(3500);
         }
-        sleep(3500);
-        slideMotorL.setPower(0);
-        slideMotorR.setPower(0);
-
-    }
 
     public void transfer1() {
         bucketServoL.setPosition(0.38);
@@ -274,22 +257,21 @@ public class autoTemplate extends LinearOpMode {
         toggleTopMethod();
     }
 
-    public void afterTransfer1() {
-        bucketServoL.setPosition(0.37);
-        bucketServoR.setPosition(0.37);
-        armServoR.setPosition(0.2);
-        armServoL.setPosition(0.2);
-        wristServo.setPosition(0.75);
-        topWrist.setPosition(0.155);
+    public void setUpAuto() {
+        bucketServoL.setPosition(0.5);
+        bucketServoR.setPosition(0.5);
+        armServoR.setPosition(0.5);
+        armServoL.setPosition(0.5);
+        topWrist.setPosition(0.5);
     }
 
     public void transfer2() {
         topClaw.setPosition(0.55);
         sleep(500);
-        topWrist.setPosition(0.73);
+        topWrist.setPosition(0.53);
         bucketServoL.setPosition(.8);
         bucketServoR.setPosition(.8);
-        sleep(1500);
+        sleep(1400);
         topClaw.setPosition(.1);
         //toggleTop = false;
         //toggleTopMethod();
@@ -315,6 +297,7 @@ public class autoTemplate extends LinearOpMode {
         fr.setPower(speed);
         br.setPower(speed);
     }
+
 }
 
 
