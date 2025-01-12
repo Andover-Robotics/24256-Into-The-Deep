399cbfed2f1d168c108964a921ac8ac5203a4819
auspicious-avery <averyaslaughter@gmail.com>
2025-01-11 22:55:53 -0500

    the objective best subsystems ever

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/IntakeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/IntakeArm.java
@@ -3,14 +3,20 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class IntakeArm {
-    private Servo armServoR, armServoL, claw, wristServo;
-    private double transferPos, intakePos, clawOpen, clawClose;
+    public Servo armServoR, armServoL;
+    public static final double transferPos = 0.42;
+    public static final double intakePos = 0.325;
 
     public IntakeArm(OpMode opMode) {
-        armServoR = opMode.hardwareMap.get(Servo.class,"armServoR");
-        armServoL = opMode.hardwareMap.get(Servo.class,"armServoL");
-        claw = opMode.hardwareMap.get(Servo.class,"claw");
-        wristServo = opMode.hardwareMap.get(Servo.class,"wristServo");
+        armServoR = opMode.hardwareMap.get(Servo.class, "armServoR");
+        armServoL = opMode.hardwareMap.get(Servo.class, "armServoL");
     }
-
+    public void armToTransferPos () {
+        armServoL.setPosition(transferPos);
+        armServoR.setPosition(transferPos);
+    }
+    public void armToIntakePos () {
+        armServoL.setPosition(intakePos);
+        armServoR.setPosition(intakePos);
+    }
 }
