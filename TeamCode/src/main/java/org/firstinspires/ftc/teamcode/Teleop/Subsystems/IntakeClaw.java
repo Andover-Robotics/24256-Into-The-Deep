3ab89e242bac3280eb57e6dc9277463cf78cf205
afriedman26 <arifriedman26@gmail.com>
2025-01-11 23:01:10 -0500

    Finished organizing arms, claws, and wrists into subsystems.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/IntakeClaw.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Teleop.Subsystems;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+@TeleOp
+public class IntakeClaw {
+    public Servo  claw, wristServo;
+
+    public static final double clawOpen = 0.05;
+    public static final double clawClose = 0.54;
+    public static final double wristUp = 0.18;
+    public static final double wristDown = 0.53;
+
+    public IntakeClaw(OpMode opMode) {
+        claw = opMode.hardwareMap.get(Servo.class,"claw");
+        wristServo = opMode.hardwareMap.get(Servo.class,"wristServo");
+    }
+
+    public void openClaw(){
+        claw.setPosition(clawOpen);
+    }
+    public void closeClaw(){
+        claw.setPosition(clawClose);
+    }
+    public void wristToTransferPos(){
+        wristServo.setPosition(wristUp);
+    }
+    public void wristToIntakePos(){
+        wristServo.setPosition(wristDown);
+    }
+
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/OuttakeClaw.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Teleop.Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class OuttakeClaw {
+    public Servo topClaw, topWrist;
+    public static final double topClawOpen = 0.1;
+    public static final double topClawClose = 0.6;
+    public static final double topWristUp = 0.53;
+    public static final double topWristDown = 0.12;
+
+    public OuttakeClaw(OpMode opMode){
+    topClaw = opMode.hardwareMap.get(Servo.class, "topClaw");
+    topWrist = opMode.hardwareMap.get(Servo.class,"topWrist");
+    }
+    public void outtakeClawClose(){
+        topClaw.setPosition(topClawClose);
+    }
+    public void outtakeClawOpen(){
+        topClaw.setPosition(topClawOpen);
+    }
+    public void topWristTransferPos(){
+        topWrist.setPosition(topWristDown);
+    }
+    public void topWristToOuttakePos(){
+        topWrist.setPosition(topWristUp);
+    }
+}
