35461517252122692fee61d05e1221109bec74fe
MichaelF1212 <mfaulise2028@student.andoverma.us>
2025-02-22 19:42:07 -0500

    send help

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/OuttakeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/OuttakeArm.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 public class OuttakeArm {
     public Servo bucketServoR, bucketServoL;
-    public static final double transfer = .205;
+    public static final double transfer = .2050;
     public static final double outtake = .77;
     public static final double vertical = .49;
     public static final double wallIntake = .995;
