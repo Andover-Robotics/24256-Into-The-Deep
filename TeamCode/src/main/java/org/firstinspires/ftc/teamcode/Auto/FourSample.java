3627c66af8d549c34b2693ff7005b6140acbfca6
MichaelF1212 <mfaulise2028@student.andoverma.us>
2025-03-27 07:08:08 -0400

    Added aftertimes to all clips, fixed third specimen and made bucket auto readable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSample.java
@@ -23,45 +23,42 @@ public class FourSample extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         Action runAuto = drive.actionBuilder(new Pose2d(38,63, Math.toRadians(-90)))
-                //go to bucket
                 .afterTime(0.001, bot.savingBottomWrist())
                 .afterTime(0.4,   bot.actionHighBucket())
                 .strafeToLinearHeading(new Vector2d(45, 61), Math.toRadians(-90))
-                //drop in bucket
                 .splineToLinearHeading(new Pose2d(57.5,61,Math.toRadians(-135)),Math.toRadians(135))
                 .afterTime(0.01, bot.actionBucketDropAuto())
+                //Outtake preload
                 .afterTime(0.000000000000001, bot.toIntake())
                 .waitSeconds(.2)
-                //intake first sample
                 .strafeToLinearHeading(new Vector2d(44.5,46),Math.toRadians(-90)) // stop and add close claw and transfer etc
                 .waitSeconds(.1)
                 .stopAndAdd(bot.intakeAuto())
-                //
+                //intake second sample
                 .afterTime(0.01, bot.actionHighBucket())
                 .strafeToLinearHeading(new Vector2d(58, 61.75), Math.toRadians(-135))
                 .stopAndAdd(bot.actionBucketDropAuto())
-//
+                //outtake second sample
                 .strafeToLinearHeading(new Vector2d(58,45.5),Math.toRadians(-90)) // stop and add close claw and transfer etc
                 .stopAndAdd(bot.intakeAuto())
-
+                //intake third sample
                 .afterTime(0.01, bot.actionHighBucket())
                 .strafeToLinearHeading(new Vector2d(56, 65), Math.toRadians(-145))
                 .stopAndAdd(bot.actionBucketDropAuto())
-
+                //outtake third sample
                 .strafeToLinearHeading(new Vector2d(58,42),Math.toRadians(-50)) // stop and add close claw and transfer etc
                 .stopAndAdd( bot.autoLastSample())
-                // stop and add close claw and transfer etc
-
-
+                //intake fourth sample
                 .afterTime(0.01, bot.actionHighBucket())
                 .strafeToLinearHeading(new Vector2d(58, 61.75), Math.toRadians(-135))
                 .stopAndAdd(bot.actionBucketDropAuto())
-
+                //outtake fourth sample
                 .splineToLinearHeading(new Pose2d(33,17,Math.toRadians(180)),Math.toRadians(-180))
                 .splineToLinearHeading(new Pose2d(11,17,Math.toRadians(180)),Math.toRadians(-180))
                 .stopAndAdd(bot.actionPark())
                 .waitSeconds(0.5)
                 .build();
+                //park
 
         waitForStart();
         Actions.runBlocking(
