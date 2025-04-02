5a720469e1cf5f6c5841b5ad2b8ffabf88e112ff
MichaelF1212 <mfaulise2028@student.andoverma.us>
2025-04-02 17:54:11 -0400

    Super fast transfer and better bucket auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSample.java
@@ -27,7 +27,7 @@ public class FourSample extends LinearOpMode {
                 .afterTime(0.4,   bot.actionHighBucketAuto())
                 .strafeToLinearHeading(new Vector2d(45, 61), Math.toRadians(-90))
                 .splineToLinearHeading(new Pose2d(58.5,62,Math.toRadians(-135)),Math.toRadians(135))
-                .afterTime(0.01, bot.actionBucketDropAuto())
+                .afterTime(0.7, bot.actionBucketDropAuto())
                 //Outtake preload
                 .afterTime(0.000000000000001, bot.toIntake())
                 .waitSeconds(.2)
@@ -37,24 +37,24 @@ public class FourSample extends LinearOpMode {
                 //intake second sample
                 .afterTime(0.01, bot.actionHighBucketAuto())
                 .strafeToLinearHeading(new Vector2d(57.75, 62.75), Math.toRadians(-135))
+                .waitSeconds(0.7)
                 .stopAndAdd(bot.actionBucketDropAuto())
-                .waitSeconds(0.2)
                 //outtake second sample
                 .strafeToLinearHeading(new Vector2d(57,43.5),Math.toRadians(-80)) // stop and add close claw and transfer etc
                 .stopAndAdd(bot.intakeAuto())
                 //intake third sample
                 .afterTime(0.01, bot.actionHighBucketAuto())
                 .strafeToLinearHeading(new Vector2d(57, 66), Math.toRadians(-145))
+                .waitSeconds(0.7)
                 .stopAndAdd(bot.actionBucketDropAuto())
-                .waitSeconds(0.2)
                 //outtake third sample
                 .strafeToLinearHeading(new Vector2d(58.5,41.5),Math.toRadians(-50)) // stop and add close claw and transfer etc
                 .stopAndAdd( bot.autoLastSample())
                 //intake fourth sample
                 .afterTime(0.01, bot.actionHighBucketAuto())
                 .strafeToLinearHeading(new Vector2d(56.5, 62.25), Math.toRadians(-148))
+                .waitSeconds(0.7)
                 .stopAndAdd(bot.actionBucketDropAuto())
-                .waitSeconds(0.15)
                 //outtake fourth sample
                 .splineToLinearHeading(new Pose2d(33,17,Math.toRadians(180)),Math.toRadians(-180))
                 .splineToLinearHeading(new Pose2d(11,17,Math.toRadians(180)),Math.toRadians(-180))
